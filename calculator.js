7dc3a046aaabdba84b6112674e60046a99f20955
Noelle <ferrari.noelle@gmail.com>
2022-07-07 23:33:09 -0700

    Add logic for basic operations

---
+++ calculator.js
@@ -0,0 +1,28 @@
+function add(a,b) {
+    return a + b
+}
+
+function subtract(a,b) {
+    return a - b
+}
+
+function multiply(a,b) {
+    return a * b
+}
+
+function divide(a,b) {
+    return a / b
+}
+
+function operate(operator,a,b) {
+    switch(operator) {
+        case '+':
+            return add(a,b)
+        case '-':
+            return subtract(a,b)
+        case '*':
+            return multiply(a,b)
+        case '/':
+            return divide(a,b)
+    }
+}
