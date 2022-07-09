2bce82a8ff4a4555527ab482e2555a46632dda5a
Noelle <ferrari.noelle@gmail.com>
2022-07-08 22:54:43 -0700

    Add logic for operator buttons

--- calculator.js
+++ calculator.js
@@ -1,28 +1,90 @@
-function add(a,b) {
-    return a + b
+const numKey = document.querySelectorAll('.num')
+let textbox = document.querySelector('.textbox')
+const mathKey = document.querySelectorAll('.math')
+const equalsKey = document.querySelector('#equals').addEventListener('click', doMath)
+const clearKey = document.querySelector('#clear').addEventListener('click', clear)
+const delKey = document.querySelector('#backspace').addEventListener('click', backspace)
+
+let display = []
+let firstValue = ''
+let operator
+let restart = false
+
+numKey.forEach((num) => {
+    num.addEventListener('click', numPress)
+})
+
+mathKey.forEach((key) => {
+    key.addEventListener('click', startMath)
+})
+
+function clear() {
+    display = []
+    operator = ''
+    firstValue = ''
+    textbox.textContent = '0'
 }
 
-function subtract(a,b) {
-    return a - b
+function backspace() {
+    if(textbox.textContent != '0') {
+        display.pop()
+        textbox.textContent = display.join('')
+    }
 }
 
-function multiply(a,b) {
-    return a * b
+function numPress(e) {
+    if(restart === true) {
+        firstValue = ''
+        restart = false
+        display.push(e.target.textContent)
+        textbox.textContent = display.join('')
+    } else if(textbox.textContent != '0') {
+        display.push(e.target.textContent)
+        textbox.textContent = display.join('')
+    } else {
+        display.pop()
+        display.push(e.target.textContent)
+        textbox.textContent = display.join('')
+    }
+
+    console.log(firstValue)
+    console.log(display)
+
 }
 
-function divide(a,b) {
-    return a / b
+function startMath(e) {
+    if(firstValue === '') {
+        firstValue = Number(display.join(''))
+        operator = e.target.id
+        display = []
+    } else {
+        doMath()
+        operator = e.target.id
+    }
 }
 
 function operate(operator,a,b) {
     switch(operator) {
-        case '+':
-            return add(a,b)
-        case '-':
-            return subtract(a,b)
-        case '*':
-            return multiply(a,b)
-        case '/':
-            return divide(a,b)
+        case 'add':
+            return a + b
+        case 'subtract':
+            return a - b
+        case 'multiply':
+            return a * b
+        case 'divide':
+            return a / b
     }
+}
+
+function doMath() {
+    if(operator != '') {
+        let answer = operate(operator, firstValue, Number(display.join('')))
+        display = Array.from(String(answer))
+        textbox.textContent = display.join('')
+        firstValue = Number(display.join(''))
+        display = []
+        operator = ''
+        restart = true
+    }
+
 }
